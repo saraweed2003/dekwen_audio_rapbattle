497538c235e464b234daaaec56a40dee52114f48
sarawut.2003 <sarawutsriprachai@gmail.com>
2023-09-30 13:47:02 +0700

    updated login 30092023

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -32,22 +32,43 @@
 
 <script setup>
 import { ref } from "vue";
+import axios from "axios";
 
 const username = ref("");
 const password = ref("");
 const showErrorMessage = ref(false);
 
-const login = () => {
-  if (username.value === "admin" && password.value === "1234") {
-    // Successful login logic here
-    console.log("Logged in successfully");
+// const login = () => {
+//   if (username.value === "admin" && password.value === "1234") {
+//     // Successful login logic here
+//     console.log("Logged in successfully");
+//     showErrorMessage.value = false;
+//   } else {
+//     showErrorMessage.value = true;
+//   }
+// };
+
+const login = async () => {
+  try {
+    const response = await axios.post(
+      "https://dekwen-audio-battle-api.billowdev.com/api/login",
+      {
+        username: username.value,
+        password: password.value,
+      }
+    );
+
+    // Handle the response from the API here
+    console.log("API Response:", response.data);
     showErrorMessage.value = false;
-  } else {
+    // You can also use Vue Router to navigate to a different page on successful login
+    // router.push('/dashboard');
+  } catch (error) {
+    // Handle any errors that occurred during the request
+    console.error("Error:", error);
     showErrorMessage.value = true;
   }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
